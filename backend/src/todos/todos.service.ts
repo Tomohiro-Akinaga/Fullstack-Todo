65c9e24b5925c17ab09e265bac131177f308aa05
Tomohiro-Akinaga <akitomo129@gmail.com>
2025-05-05 01:05:45 +0800

    feat: backend api実装

---
+++ backend/src/todos/todos.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+import { CreateTodoDto } from './dto/create-todo.dto';
+import { UpdateTodoDto } from './dto/update-todo.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class TodosService {
+  constructor(private prisma: PrismaService) {}
+
+  create(createTodoDto: CreateTodoDto) {
+    return createTodoDto;
+  }
+
+  findAll() {
+    return this.prisma.todo.findMany();
+  }
+
+  findOne(id: number) {
+    return this.prisma.todo.findUnique({ where: { id } });
+  }
+
+  update(id: number, updateTodoDto: UpdateTodoDto) {
+    return updateTodoDto;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} todo`;
+  }
+}
