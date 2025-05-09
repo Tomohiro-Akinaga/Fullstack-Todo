05023eed6e4dd784f17d1b7ad04549c4d99e07cc
Tomohiro-Akinaga <akitomo129@gmail.com>
2025-05-09 09:16:26 +0800

    WIP

--- frontend/components/TodoForm.tsx
+++ frontend/components/TodoForm.tsx
@@ -1,11 +1,13 @@
 import { Button } from "@/components/ui/Button";
 import { Input } from "@/components/ui/Input";
 
+import { createTodo } from "@/app/todos/actions";
+
 export function TodoForm() {
   return (
-    <div className="flex gap-2">
+    <form className="flex gap-2" action={createTodo}>
       <Button className="cursor-pointer">+</Button>
       <Input />
-    </div>
+    </form>
   );
 }
